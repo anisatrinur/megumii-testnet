c04607978319cb2680d01c148bf480cc2e2c1a5a
anisatrinur <137194422+anisatrinur@users.noreply.github.com>
2024-03-21 22:41:35 +0700

    Update run.sh
    Change RPC

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-38b7746b-60bf-48dc-88d6-c65d172f0e8f/)
 
 
 echo "$curl_output" >> Update.tsx
